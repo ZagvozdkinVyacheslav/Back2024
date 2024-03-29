044a7f8789f22b2a59ee163fe7f7af9f480fda4a
v.zagvozdkin <v.zagvozdkin@cloudcom.ru>
2024-03-29 20:21:10 +0300

    Initial commit

---
+++ src/main/java/org/tmpk/back2024/Back2024Application.java
@@ -0,0 +1,13 @@
+package org.tmpk.back2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Back2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/tmpk/back2024/Back2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tmpk.back2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Back2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
